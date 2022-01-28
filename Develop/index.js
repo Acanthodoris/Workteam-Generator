11038d5e4adca77b0bfb671da6fcdf3b46fc7e12
Becca <95663074+Acanthodoris@users.noreply.github.com>
2022-01-27 23:10:28 -0800

    Completed team member quiz questions

---
+++ Develop/index.js
@@ -0,0 +1,135 @@
+const inquirer = require("inquirer");
+const fs = require('fs');
+
+const Employee = require("./lib/Employee");
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+
+const generate = require("./util/generateHtml");
+const team = []
+
+
+
+console.log('Lets start building your team!')
+  inquirer
+    .prompt([
+      {
+        type: 'input',
+        message: 'What is your managers name?',
+        name: 'name',
+      },
+      {
+        type: 'imput',
+        message: 'What is your managers ID number?',
+        name: 'id',
+      },
+      {
+        type: 'input',
+        message: 'What your managers email address?',
+        name: 'email',
+      },
+      {
+        type: 'input',
+        message: 'What your managers office number?',
+        name: 'officeNumber',
+      }
+  ])
+  .then(answers => {
+      let manager = new Manager(answers.name, answers.id, answers.email, answers.officeNumber);
+      team.push(manager);
+      teamBuilding();
+  });
+
+function teamBuilding() {
+  inquirer
+    .prompt([
+      {
+        type: 'list',
+        message: 'Would you like to add on to your team?',
+        name: 'team',
+        choices: ['Add a Engineer', 'Add a Intern', 'All done!'],
+        loop: true
+      },
+    ])
+  .then(answers => {
+    switch (answers.team) {
+      case 'Add a Engineer':
+        engineerQuestions()
+        break;
+    
+      case 'Add a Intern':
+        internQuestions()
+        break;
+
+      case 'All done!':
+        // new function to end questions
+        break;
+
+      default:
+        break;
+    }
+})
+};
+
+function engineerQuestions() {
+  inquirer
+    .prompt([
+      {
+        type: 'input',
+        message: 'What is your engineers name?',
+        name: 'name',
+      },
+      {
+        type: 'imput',
+        message: 'What is your engineers ID number?',
+        name: 'id',
+      },
+      {
+        type: 'input',
+        message: 'What your engineers email address?',
+        name: 'email',
+      },
+      {
+        type: 'input',
+        message: 'What your engineers GitHub username?',
+        name: 'github',
+      }
+  ])
+  .then(answers => {
+      let engineer = new Engineer(answers.name, answers.id, answers.email, answers.github);
+      team.push(engineer);
+      teamBuilding()
+  });
+};
+
+function internQuestions() {
+  inquirer.prompt([
+    {
+      type: 'input',
+      message: 'What is your interns name?',
+      name: 'name',
+    },
+    {
+      type: 'imput',
+      message: 'What is your interns ID number?',
+      name: 'id',
+    },
+    {
+      type: 'input',
+      message: 'What your interns email address?',
+      name: 'email',
+    },
+    {
+      type: 'input',
+      message: 'What school did your intern attend?',
+      name: 'school',
+    }
+  ])
+  .then(answers => {
+    let intern = new Intern(answers.name, answers.id, answers.email, answers.school);
+    team.push(intern);
+    teamBuilding()
+  });
+}
+
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Workteam-Generator
 Building a Node.js command-line application that takes in information about employees on a software engineering team, then generates an HTML webpage that displays summaries for each person.
+
+
+## Credits:
+* https://stackoverflow.com/questions/56412516/conditional-prompt-rendering-in-inquirer
